6ab4395beeafcdc3a493386ada3f3ccc88e2c67a
Amankamboj04 <kambojaman49485@gmail.com>
2026-03-12 23:07:57 +0530

    Displayed Square and Cude of Number

---
+++ Some-basic-programs/SquareCube.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class SquareCube {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        int square = num * num;
+        int cube = num * num * num;
+
+        System.out.println("Square = " + square);
+        System.out.println("Cube = " + cube);
+
+        sc.close();
+    }
+}
