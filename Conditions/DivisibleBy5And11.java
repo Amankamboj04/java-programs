21d190d0abe7c596e7e06ec7492b119e608997cb
Amankamboj04 <kambojaman49485@gmail.com>
2026-03-13 22:38:05 +0530

    Checked number divisible by 5 and 11

---
+++ Conditions/DivisibleBy5And11.java
@@ -0,0 +1,18 @@
+package Conditions;
+import java.util.Scanner;
+public class DivisibleBy5And11 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        if(num % 5 == 0 && num % 11 == 0){
+            System.out.println("Divisible by 5 and 11");
+        }
+        else{
+            System.out.println("Not divisible");
+        }
+
+        sc.close();
+    }
+}
