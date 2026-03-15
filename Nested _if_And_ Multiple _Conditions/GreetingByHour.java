497ce6c6e1bab082fa0f28c4fc080487f0aa146a
Amankamboj04 <kambojaman49485@gmail.com>
2026-03-15 13:34:50 +0530

    Displayed greeting based on hour (0 - 23)

---
+++ Nested _if_And_ Multiple _Conditions/GreetingByHour.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class GreetingByHour{
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Clock time from (0 to 23):");
+        int a = sc.nextInt();
+
+        if(a < 0 || a > 23){
+            System.out.println("Enter valid clock hour.");
+        }
+        else if(a <= 11){
+            System.out.println("Good Morning!!!");
+        }
+        else if(a <= 16){
+            System.out.println("Good Afternoon!!!");
+        }
+        else if(a <= 19){
+            System.out.println("Good Evening!!!");
+        }
+        else{
+            System.out.println("Good Night!!!");
+        }
+
+        sc.close();
+    }
+}
