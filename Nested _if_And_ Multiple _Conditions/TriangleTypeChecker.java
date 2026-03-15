9df4d18f7a553c66b49702e8f72f2ebc48dee521
Amankamboj04 <kambojaman49485@gmail.com>
2026-03-15 11:29:34 +0530

    checked triangle type (Equilateral, Isosceles, Scalene)

---
+++ Nested _if_And_ Multiple _Conditions/TriangleTypeChecker.java
@@ -0,0 +1,28 @@
+package nested _if_and_ multiple _conditions;
+import java.util.Scanner;
+public class TriangleTypeChecker {
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a side a:");
+        int a=sc.nextInt();
+        System.out.println("Enter a side b:");
+        int b=sc.nextInt();
+        System.out.println("Enter a side c:");
+        int c=sc.nextInt();
+        if(a+b>c && a+c>b && b+c>a){
+            System.out.println("Valid Triangle.");
+            if(a==b && b==c && a==c){
+                System.out.println("Equilateral Triangle.");
+            }
+            else if(a==b || b==c ||a==c){
+                System.out.println("Isosceles triangle.");
+            }
+            else if(a !=b && b!=c && c!=a){
+                System.out.println("Scalene Triangle.");
+            }
+        }
+        else{
+            System.out.println("Not Valid triangle.");
+        }
+    }
+}
