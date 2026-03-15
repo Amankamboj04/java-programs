e71253b75550d52cae0a17099abaadc5bf8552f7
Amankamboj04 <kambojaman49485@gmail.com>
2026-03-15 10:53:08 +0530

    Rename Directory

---
+++ Nested _if_And_ Multiple _Conditions/CheckValidTriangle.java
@@ -0,0 +1,20 @@
+package Nested_if_And_Multiple_Conditions;
+import java.util.Scanner;
+public class CheckValidTriangle {
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a side a:");
+        int a=sc.nextInt();
+        System.out.println("Enter a side b:");
+        int b=sc.nextInt();
+        System.out.println("Enter a side c:");
+        int c=sc.nextInt();
+        if(a+b>c && a+c>b && b+c>a){
+            System.out.println("Valid Triangle.");
+        }
+        else{
+            System.out.println("Not valid triangle.");
+        }
+    }
+    
+}
