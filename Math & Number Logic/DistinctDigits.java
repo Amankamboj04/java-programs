796d776e969d4945ae568cca737a5d6e3c23af41
Aman kamboj <kambojaman49485@gmail.com>
2026-03-16 23:00:55 +0530

    Added new folder in java programs

---
+++ Math & Number Logic/DistinctDigits.java
@@ -0,0 +1,19 @@
+//Take a 3-digit number and check if all digits are distinct. 
+import java.util.Scanner;
+public class DistinctDigits {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a 3 digits number:");
+        int num=sc.nextInt();
+        int first=num/100;
+        int second=(num/10)%10;
+        int third=num%10;
+        if(first != second && second != third && third != first ){
+            System.out.println("All digits are distinct.");
+        }
+        else{
+            System.out.println("All digits are not distinct.");
+     
+     sc.close();   }
+    }
+}
