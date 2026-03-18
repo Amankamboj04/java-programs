01b6e4120e5cbe81dba0bb549efddb4459ffd783
Aman kamboj <kambojaman49485@gmail.com>
2026-03-18 21:15:41 +0530

    Compute third angle of triangle ensuring valid input

---
+++ Math & Number Logic/ComputeThirdAngle.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+public class ComputeThirdAngle {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter first angle value:");
+        int a=sc.nextInt();
+        System.out.println("Enter second angle value:");
+        int b=sc.nextInt();
+        if(a > 0 && b > 0 && a + b < 180){
+            int thirdAngle = 180 - (a + b);
+            System.out.println("Third angle is: " + thirdAngle);
+            } else {
+            System.out.println("Invalid angles. Angles must be positive and sum less than 180.");            }        
+        sc.close();
+    }
+}
